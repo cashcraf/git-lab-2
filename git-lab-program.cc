8bc4746d3233db1b36dca103f0e4ce4da1c8462b
cashcraf <ca242521@ohio.edu>
2023-01-24 11:48:13 -0500

    My final step

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Colton Ashcraft
+ *        Date: 01-24-2023
+ * Description: This is my code for my lab2 in c++
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
